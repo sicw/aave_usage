8536cf27f284f4695bac868d6c7de9eaf1667f8e
sichengwen <sichengwen@kuaishou.com>
2023-07-03 18:23:10 +0800

    add type

--- test/aave_v3.ts
+++ test/aave_v3.ts
@@ -7,6 +7,7 @@ import PoolV3Artifact from "@aave/core-v3/artifacts/contracts/protocol/pool/Pool
 // import { AaveV2Avalanche } from "@bgd-labs/aave-address-book";   // Unknown file extension ".ts"
 import {POOL} from "./AaveV3ArbitrumConstants"
 import {AaveV3ArbitrumAssets_DAI_UNDERLYING} from "./AaveV3ArbitrumAssetsConstants"
+import {DataTypes} from "@aave/core-v3/dist/types/types/protocol/pool/Pool";
 
 describe("AAVE", function () {
 
@@ -50,8 +51,8 @@ describe("AAVE", function () {
     describe("supply logic", function () {
         it.skip("getReserveData", async function () {
             const {pool} = await loadFixture(deployAAVEProtocolFixture);
-            const reserveData = await pool.getReserveData(AaveV3ArbitrumAssets_DAI_UNDERLYING);
-            console.log(reserveData);
+            const reserveData: DataTypes.ReserveDataStruct = await pool.getReserveData(AaveV3ArbitrumAssets_DAI_UNDERLYING);
+            console.log(`DAI aTokenAddress:${reserveData.aTokenAddress}`);
         });
     });
 });
