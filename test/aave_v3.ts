c93aa610a0f03dea87521d81e0f76fc191fa4561
sichengwen <sichengwen@kuaishou.com>
2023-07-03 20:06:45 +0800

    add supply

--- test/aave_v3.ts
+++ test/aave_v3.ts
@@ -4,10 +4,17 @@ import {
 import {ethers} from "hardhat";
 import hre from "hardhat";
 import PoolV3Artifact from "@aave/core-v3/artifacts/contracts/protocol/pool/Pool.sol/Pool.json";
+import L2PoolV3Artifact from "@aave/core-v3/artifacts/contracts/protocol/pool/L2Pool.sol/L2Pool.json";
+import L2EncoderV3Artifact from "@aave/core-v3/artifacts/contracts/misc/L2Encoder.sol/L2Encoder.json";
 // import { AaveV2Avalanche } from "@bgd-labs/aave-address-book";   // Unknown file extension ".ts"
-import {POOL} from "./AaveV3ArbitrumConstants"
-import {AaveV3ArbitrumAssets_DAI_UNDERLYING} from "./AaveV3ArbitrumAssetsConstants"
+import {POOL, L2_ENCODER} from "./AaveV3ArbitrumConstants"
+import {
+    AaveV3ArbitrumAssets_DAI_UNDERLYING,
+    AaveV3ArbitrumAssets_MAI_UNDERLYING,
+    AaveV3ArbitrumAssets_WETH_UNDERLYING
+} from "./AaveV3ArbitrumAssetsConstants"
 import {DataTypes} from "@aave/core-v3/dist/types/types/protocol/pool/Pool";
+import {formatUnits, parseEther} from "ethers";
 
 describe("AAVE", function () {
 
@@ -19,7 +26,9 @@ describe("AAVE", function () {
         });
         const signer = await ethers.provider.getSigner(mockAccount);
         const pool = new ethers.Contract(POOL, PoolV3Artifact.abi, signer);
-        return {pool, signer};
+        const l2pool = new ethers.Contract(POOL, L2PoolV3Artifact.abi, signer);
+        const L2Encoder = new ethers.Contract(L2_ENCODER, L2EncoderV3Artifact.abi, signer);
+        return {pool, l2pool, L2Encoder, signer};
     }
 
     describe.skip("Before Test", function () {
@@ -53,6 +62,21 @@ describe("AAVE", function () {
             const {pool} = await loadFixture(deployAAVEProtocolFixture);
             const reserveData: DataTypes.ReserveDataStruct = await pool.getReserveData(AaveV3ArbitrumAssets_DAI_UNDERLYING);
             console.log(`DAI aTokenAddress:${reserveData.aTokenAddress}`);
+        });
+
+        it.skip("L2Encoder", async function () {
+            const {pool, L2Encoder} = await loadFixture(deployAAVEProtocolFixture);
+            const supplyEth = parseEther('0.01');
+            const params = await L2Encoder.encodeSupplyParams(AaveV3ArbitrumAssets_WETH_UNDERLYING, supplyEth, 0);
+            console.log(`params:${params}`);
+        });
+
+        it("supply", async function () {
+            const {l2pool, L2Encoder} = await loadFixture(deployAAVEProtocolFixture);
+            const supplyEth = parseEther('0.01');
+            const params = await L2Encoder.encodeSupplyParams(AaveV3ArbitrumAssets_MAI_UNDERLYING, supplyEth, 0);
+            const resp = await l2pool.supply(params);
+            console.log(`supply:${JSON.stringify(resp)}`);
         });
     });
 });
